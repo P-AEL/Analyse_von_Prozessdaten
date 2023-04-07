f14eac94e185752f015e5f0ca0b6d69726ccfeb7
klfl1012 <florian.klein02@gmx.net>
2023-04-07 15:54:48 +0200

    kjasdbasd

---
+++ setup/data_load.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import os.path
+
+file_name= "SmA-Four-Tank-Batch-Process_V2.csv"
+
+if not os.path.isfile(file_name):
+ print("Missing file {}.".format(file_name))
+ exit()
+df= pd.read_csv(file_name, sep= ";")
+
+if df.columns.isin(["DeviationID ValueY", "timestamp"]).any():
+ df["timestamp"]= pd.to_datetime(df["timestamp"], format= "%Y-%m-%d %H:%M:%S")
+ print("Found Data, Number of deviations: {}".format(len(df["DeviationID ValueY"].unique())))
